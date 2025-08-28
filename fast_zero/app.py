725c40ba7a8c35e0a19d610f99f780af312c1b23
vinicodex <vinicodex@gmail.com>
2025-08-28 17:37:13 -0300

    feat: add testing to update api

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -12,11 +13,31 @@ def read_root():
     return {'message': 'Olá, Mundo!'}
 
 
-database = []
-
-
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user)
+    return user
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+    {
+        'username': 'string2',
+        'email': 'user@example.com',
+        'id': 1,
+    }
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user
     return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -22,3 +22,48 @@ def test_create_user(client):
         'email': 'user@example.com',
         'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'string2',
+                'email': 'user@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_should_raise_exception(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'Marcos',
+            'email': 'marcos@example.com',
+            'password': 'marcos',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Marcos',
+            'email': 'marcos@example.com',
+            'password': 'marcos',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Marcos',
+        'email': 'marcos@example.com',
+        'id': 1,
+    }
